d0d08e9bc9df9e1f385cd4a88d26e1974a02494a
Anton <anton@sobes.io>
2017-12-31 02:43:31 +0000

    src and test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-/**
- * Invoke package's main function
- */
-function assertThrows() {
-    console.log('assert-throws called')
+const equal = (a, b) => {
+    if (a != b) throw new Error(`${a} != ${b}`)
+}
+async function assertThrows ({ fn, message, args = [], context = null }) {
+    if (typeof fn !== 'function') throw new Error('function expected')
+    if (message && typeof message !== 'string') {
+        throw new Error('please pass an error message as a string')
+    }
+
+    const shouldHaveThrownError = new Error(`${fn} should have thrown`)
+    try {
+        await fn.call(context, ...args)
+        throw shouldHaveThrownError
+    } catch (err) {
+        if (err === shouldHaveThrownError) {
+            throw err
+        }
+        if (message) {
+            equal(err.message, message)
+        }
+    }
 }
 
 module.exports = assertThrows
--- test/spec/index.js
+++ test/spec/index.js
@@ -1,15 +1,50 @@
 const assert = require('assert')
 const context = require('../context/')
-const assertThrows = require('../../src/')
+const assertThrows = require('../..')
+
+const { equal } = assert
 
 const assertThrowsTestSuite = {
     context,
     'should be a function': () => {
         assert.equal(typeof assertThrows, 'function')
     },
-    'should call package without error': () => {
-        assert.doesNotThrow(() => {
-            assertThrows()
+    async 'should call the error function'() {
+        let called = false
+        await assertThrows({
+            async fn() {
+                called = true
+                throw new Error('test-error')
+            },
+        })
+        assert(called)
+    },
+    async 'should compare error messages'() {
+        await assertThrows({
+            async fn() {
+                throw new Error('test-error')
+            },
+            message: 'test-error',
+        })
+    },
+    async 'should throw when comparing error messages'() {
+        try {
+            await assertThrows({
+                async fn() {
+                    throw new Error('test-error1')
+                },
+                message: 'test-error',
+            })
+        } catch ({ message }) {
+            equal(message, 'test-error1 != test-error')
+        }
+    },
+    async 'should work with sync function'() {
+        await assertThrows({
+            fn() {
+                throw new Error('test-error')
+            },
+            message: 'test-error',
         })
     },
 }
